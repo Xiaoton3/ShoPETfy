5ed54782bdedf902b013618fa240231abebab580
debbie-chan <71258354+debbie-chan@users.noreply.github.com>
2022-10-01 17:52:01 -0400

    Create funct_1.py

---
+++ funct_1.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objs as go
+
+
+pet_df = pd.read_csv('data/pet_characteristics/pet_cleaned.csv')
